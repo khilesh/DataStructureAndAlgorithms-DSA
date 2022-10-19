1b901d2d15e0aa60eff5e96e4e14ce4e818e13a8
Nerdisworking <Nerdisworking@gmail.com>
2022-10-19 00:04:38 -0700

    Bit operations

---
+++ BitManipulation/src/main/java/org/bit/manipulation/ShiftOperators.java
@@ -0,0 +1,5 @@
+package org.bit.manipulation;
+
+public class ShiftOperators {
+
+}
---
+++ BitManipulation/src/main/java/org/bit/manipulation/XOR1.java
@@ -0,0 +1,19 @@
+package org.bit.manipulation;
+
+public class XOR1 {
+
+
+public static void main (String[] args){
+	
+	
+	int intOne = 1;
+	int intTwo = 2;
+	
+	String stringOne = "1";
+	String stringTwo = "2";
+	
+	System.out.println("Hellow World " + (intOne ^ intTwo));
+	
+	System.out.println("Hello  World " + (Integer.parseInt(stringOne) ^ Integer.parseInt( stringTwo)));
+}
+}
