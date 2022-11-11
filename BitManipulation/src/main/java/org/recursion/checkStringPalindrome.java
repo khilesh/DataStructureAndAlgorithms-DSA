cc75238e8e7ff112db85d5e3a75e5ed9841ffd6a
Nerdisworking <Nerdisworking@gmail.com>
2022-11-11 14:45:48 -0700

    String + recursion

--- BitManipulation/src/main/java/org/recursion/checkStringPalindrome.java
+++ BitManipulation/src/main/java/org/recursion/checkStringPalindrome.java
@@ -7,9 +7,15 @@ public static void main(String[] args){
 	
 	//String sample = "My name is lakhan";
 	
-	String sample = "abcba"; //"racecar";
+	String sample = "abc"; //"racecar";
 	
-	System.out.println("Check palindrome :: " + isPalindrome(sample, 0, sample.length() -1));
+	//System.out.println("Check palindrome :: " + isPalindrome(sample, 0, sample.length() -1));
+	
+	//System.out.println("Check palindrome :: "  + printPowerSet(sample, 0, ""));
+	
+	//printPowerSet(sample, 0, "");
+	
+	printPermutation(sample, "");
 }
 
 /***WRONG LOGIC**/
@@ -33,5 +39,51 @@ public static boolean isPalindrome(String input, int startIndex, int endIndex){
 	
 }
 
+/***** WRONG APPROACH
+public static int printPowerSet(String input, int startIndex, String subSet){
+	
+	if(startIndex >= input.length()) { System.out.println(" Current String " + subSet);}
+	
+	printPowerSet(input, startIndex+1, subSet );
+	printPowerSet(input, startIndex+1, input.substring(0, startIndex+1) );
+	return 0;
+}
+ ****/
 
+/**
+ * input: Input String
+ * current is current subset string
+ * startIndex is Index in current substring
+ */
+
+public static void  printPowerSet(String input, int startIndex, String current){
+
+	int n = input.length();
+	
+	if(startIndex == n) { return ;} // Base case
+	
+	System.out.println("Current :: " +current);
+	
+	for(int i=startIndex+1; i< n; i++){
+		current += input.charAt(i);
+		printPowerSet(input, i, current);
+		current = current.substring(0, current.length()-1);
+	
+	}
+}
+/**
+ * function to print all the permutations of str
+ */
+public static void printPermutation(String input, String ans){
+	if(input.length() ==0){
+		System.out.println( ans + "");
+		return ;
+	}
+	for(int i=0; i<input.length(); i++){
+		char ch = input.charAt(i);
+		String restOfString = input.substring(0,i) + input.substring(i+1);
+		
+		printPermutation(restOfString, ans + ch);
+	}
+}
 }
